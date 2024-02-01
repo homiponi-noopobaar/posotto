fc427123a61b7e849897c7c44e630a84adf536a3
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2024-02-01 21:18:58 +0900

    completed speech_recognition

--- backend/lib/speech_recognition/BERT.py
+++ backend/lib/speech_recognition/BERT.py
@@ -1,5 +1,14 @@
+import speech_recognition as sr
 from transformers import pipeline, MarianMTModel, MarianTokenizer
 import torch
+
+# 音声認識オブジェクト
+recognizer = sr.Recognizer()
+
+# WAVファイルの読み込み(適宜パスの変更をお願いします)
+with sr.AudioFile('C:/Users/Green/posotto/backend/lib/speech_recognition/wav/おはようございます.wav') as source:
+    # 音声データの読み込み
+    audio_data = recognizer.record(source)
 
 # 初期化
 model_name = 'Helsinki-NLP/opus-mt-ja-en'
@@ -15,14 +24,23 @@ def translate_ja_to_en(text):
 sentiment_pipeline = pipeline('sentiment-analysis')
 
 # 日本語テキスト(ここに音声ファイルのテキスト化したものを格納する)
-ja_text = "眠いけど頑張る"
+ja_text = recognizer.recognize_google(audio_data, language="ja-JP")
 
 # 翻訳を実行
 en_text = translate_ja_to_en(ja_text)
-print(f"Translated Text: {ja_text}")
 
 # 感情分析を実行
 sentiment = sentiment_pipeline(en_text)
-print(f"Sentiment: {sentiment}")
 
-# ここに感嘆符や疑問符などを付加する関数？を記述する
+# スコアに基づいてテキストを変更
+score = sentiment[0]['score']
+if sentiment[0]['label'] == 'POSITIVE' and score >= 0.975:
+    ja_text += "！"
+elif sentiment[0]['label'] == 'NEGATIVE' and score >= 0.99:
+    ja_text += "..."
+
+# 感情値確認用
+# print(f"Sentiment: {sentiment}")
+
+#投稿内容
+print(f"{ja_text}")
