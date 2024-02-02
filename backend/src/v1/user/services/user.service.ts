98188519a373c964d58c83750653d62173adcdb8
Hiroto2002 <hiroto2002h14@icloud.com>
2024-02-02 22:27:30 +0900

    WIP:createUser

--- backend/src/v1/user/services/user.service.ts
+++ backend/src/v1/user/services/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
+import { RequestCreateUser } from '../types/User';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -7,8 +9,15 @@ export class UserService {
     private userRepository: UserRepository,
   ) {}
 
+  //todo: tokenを使っていいねの判定を行う
   async findUserbyPublicId(publicId: string) {
     const user = await this.userRepository.findProfilebyPublicId(publicId);
     return user;
   }
+
+  async createUser(user:CreateUserDto ) {
+    const createdUser = await this.userRepository.createUser(user);
+    return createdUser;
+  }
+
 }
