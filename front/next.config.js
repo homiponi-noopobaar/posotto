de2215c23bc1f0a19cf36f107b03ca7c94c6aaf6
Hiroto2002 <hiroto2002h14@icloud.com>
2024-02-03 07:36:33 +0900

    feat:pwa

---
+++ front/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+})
