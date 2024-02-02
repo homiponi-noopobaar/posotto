cd7f4270bf519d59ce496a49f9e51e96f2c0ab3f
teramon1031 <mio.terakado@styz.io>
2024-02-03 01:46:54 +0900

    createUserとリファクタ

--- front/variants/index.ts
+++ front/variants/index.ts
@@ -1,21 +1,15 @@
-// export const ICON_BOX_SHADOW_UNPRESSED =
-//   '6px 6px 12px #cbced4, -6px -6px 12px #ffffff'
+export const BASE_COLOR = '#ebe5d9'
+export const BG_COLOR = BASE_COLOR
+export const BASE_COLOR_DARK = '#a3908d'
 
 export const ICON_BOX_SHADOW_UNPRESSED =
   '5px 5px 10px #c8c3b8,-5px -5px 10px #fffffa'
 
-// export const ICON_BOX_SHADOW_PRESSED =
-//   'inset 6px 6px 12px #cbced4, inset -6px -6px 12px #ffffff'
-
 export const ICON_BOX_SHADOW_PRESSED =
   'inset 5px 5px 10px #c8c3b8, inset -5px -5px 10px #fffffa'
 
-// export const BOX_SHADOW_UNPRESSED =
-//   '28px 28px 56px #c4c6cc,-28px -28px 56px #ffffff'
 export const BOX_SHADOW_UNPRESSED =
   '20px 20px 52px #c8c3b8,-20px -20px 52px #fffffa'
-
-export const BG_COLOR = '#ebe5d9'
 
 export const BOX_SHADOW_PRESSED =
   'inset 18px 18px 36px #c8c3b8, inset -18px -18px 36px #fffffa'
